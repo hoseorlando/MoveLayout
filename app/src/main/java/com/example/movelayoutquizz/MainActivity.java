2e88521a27d81f095c918957f12abbb357dbe6f3
Your Name <you@example.com>
2021-09-30 17:30:39 -1200

    Initial commit

---
+++ app/src/main/java/com/example/movelayoutquizz/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.movelayoutquizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void MoveLayout1(View view) {
+        Intent intent = new Intent(MainActivity.this, SignIn.class);
+        startActivity(intent);
+    }
+    public void MoveLayout2(View view) {
+        Intent intent = new Intent(MainActivity.this, SignUp.class);
+        startActivity(intent);
+    }
+}
